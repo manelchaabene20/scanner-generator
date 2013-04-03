7d39495fe1d665e9b96e68666ac3718f6d7c9338
Matt Dye <matt@lawn-128-61-25-139.lawn.gatech.edu>
2013-04-03 19:51:06 -0400

    Added a node class

--- Scanner-Generator/src/NFA.java
+++ Scanner-Generator/src/NFA.java
@@ -4,19 +4,34 @@ public class NFA {
 	private String name;
 	private int[][] NFAtable;
 	private ArrayList<Character> acceptedChars;
+	private ArrayList<Node> starterNodes;
+	NFA prev;
+	Node startState;
 
+	public NFA(String name) throws Exception{
+		this.name = name;
+		this.starterNodes = new ArrayList<Node>();
+		
+		
+	}
 	public NFA(String name, String regex) throws Exception {
 		this.name = name;
 		acceptedChars = new ArrayList<Character>();
-		NFAtable = parse(regex);
+		this.startState = parse(regex,0);
+		
+		
 	}
 
-	public NFA(String name, String regex, NFA prev) {
+	public NFA(String name, String regex, NFA prev) throws Exception {
+		this.acceptedChars = new ArrayList<Character>();
 		this.name = name;
-		// table = parse(regex);
+		this.prev = prev;
+		this.startState = parse(regex,1);
+		
+	
 	}
 
-	public int[][] parse(String regex) throws Exception {
+	public Node parse(String regex, int method) throws Exception {
 
 		/* Parsing genius code */
 		int state = 0;
@@ -26,88 +41,119 @@ public class NFA {
 		c = regex.charAt(index);
 
 		/* Figure out accepted characters in a [] character class */
-		if (c == '[') {
-			c = regex.charAt(++index);
-			boolean accepted = true;
-			if (c == '^') {
-				accepted = false;
-			}
-			while (c != ']') {
-				if (c == '-') {
-					char start = acceptedChars.get(acceptedChars.size() - 1);
-					char end = regex.charAt(index + 1);
-
-					if (start > end) {
-						throw new Exception("Range is invalid.");
-					}
-
-					for (int i = (int) start + 1; i < (int) end; i++) {
-						acceptedChars.add((char) i);
-					}
-				} else if (c == '\\') { // NOTE: have to escape the \ char in
-										// Java
-					c = regex.charAt(++index);
-					acceptedChars.add(c);
-				} else { // All other characters
-					acceptedChars.add(c);
+		if(method == 0){
+			if (c == '[') {
+				c = regex.charAt(++index);
+				boolean accepted = true;
+				if (c == '^') {
+					accepted = false;
 				}
-				if (c != ']')
-					c = regex.charAt(++index);
-			}
-			if (accepted == false) { // We have a ^
-				ArrayList<Character> actualAcceptedChars = new ArrayList<Character>();
-				for (int i = 0; i < 256; i++) { // Loop through all possible
-												// chars
-					if (!acceptedChars.contains((char) i)) {
-						actualAcceptedChars.add((char) i);
+				while (c != ']') {
+					if (c == '-') {
+						char start = acceptedChars.get(acceptedChars.size() - 1);
+						char end = regex.charAt(index + 1);
+	
+						if (start > end) {
+							throw new Exception("Range is invalid.");
+						}
+	
+						for (int i = (int) start + 1; i < (int) end; i++) {
+							acceptedChars.add((char) i);
+						}
+					} else if (c == '\\') { // NOTE: have to escape the \ char in
+											// Java
+						c = regex.charAt(++index);
+						acceptedChars.add(c);
+					} else { // All other characters
+						acceptedChars.add(c);
 					}
+					if (c != ']')
+						c = regex.charAt(++index);
 				}
-				acceptedChars = actualAcceptedChars;
+				if (accepted == false) { // We have a ^
+					ArrayList<Character> actualAcceptedChars = new ArrayList<Character>();
+					for (int i = 0; i < 256; i++) { // Loop through all possible
+													// chars
+						if (!acceptedChars.contains((char) i)) {
+							actualAcceptedChars.add((char) i);
+						}
+					}
+					acceptedChars = actualAcceptedChars;
+				}
+				index++; // Increment past ']'
 			}
-			index++; // Increment past ']'
+	
+			/* Build NFA table */
+			Node node = new Node();
+			node.addSuccessor(new Node(acceptedChars,true));
+			return node;
 		}
-
-		/* Build NFA table */
-
-		int[][] table = new int[2 + acceptedChars.size()][acceptedChars.size() + 1];
-
-		/* Labels for the columns of the table */
-		for (int x = 0; x < acceptedChars.size(); x++) {
-			char ac = acceptedChars.get(x);
-			table[0][x] = ac;
+		else if(method == 1){
+			if (c == '[') {
+				c = regex.charAt(++index);
+				boolean accepted = true;
+				if (c == '^') {
+					accepted = true;
+				}
+				while (c != ']') {
+					if (c == '-') {
+						char start = acceptedChars.get(acceptedChars.size() - 1);
+						char end = regex.charAt(index + 1);
+	
+						if (start > end) {
+							throw new Exception("Range is invalid.");
+						}
+	
+						for (int i = (int) start + 1; i < (int) end; i++) {
+							acceptedChars.add((char) i);
+						}
+					} else if (c == '\\') { // NOTE: have to escape the \ char in
+											// Java
+						c = regex.charAt(++index);
+						acceptedChars.add(c);
+					} else { // All other characters
+						acceptedChars.add(c);
+					}
+					if (c != ']')
+						c = regex.charAt(++index);
+				}
+				if (accepted == false) { // We have a ^
+					ArrayList<Character> actualAcceptedChars = new ArrayList<Character>();
+					for (int i = 0; i < 256; i++) { // Loop through all possible
+													// chars
+						if (!acceptedChars.contains((char) i)) {
+							actualAcceptedChars.add((char) i);
+						}
+					}
+					acceptedChars = actualAcceptedChars;
+				}
+				index++; // Increment past ']'
+			}
+			ArrayList<Character> largerList = this.prev.getAcceptedChars();
+			for(Character ch : acceptedChars){
+				largerList.remove(ch);
+			}
+			Node node = new Node();
+			node.addSuccessor(new Node(largerList,true));
+			return node;
 		}
-
-		/* Put state transitions in table (only first row will have them) */
-		for (int y = 0; y < table[1].length; y++) {
-			table[1][y] = y + 1;
-		}
-
-		/* Is state accepting(1) or non-accepting(0)? */
-		for (int x = 1; x < table.length; x++) {
-			table[x][table[0].length - 1] = 0;
-		}
-		table[1][table[0].length - 1] = 1;
-		return table;
-
+		else
+			return null;
 	}
 
 	public void print() {
-		for (int r = 0; r < 2; r++) {
-			for (int c = 0; c < NFAtable[0].length; c++) {
-				if (r == 0) {
-					System.out.print(((char) NFAtable[r][c]) + "  ");
-				} else {
-					System.out.print(NFAtable[r][c] + "  ");
-				}
-
-			}
-			System.out.println();
+		for(Node n : this.startState.successors){
+			System.out.println(n.nextChar);
 		}
 	}
-	/*
+	public ArrayList<Character> getAcceptedChars(){
+		return this.acceptedChars;
+	}
 	public static void main(String[] args) throws Exception {
-		NFA nfa = new NFA("$DIGIT", "[A-Z\\+\\[\\ \\\\]");
-		nfa.print();
-	}*/
+		
+		NFA nfa = new NFA("$CHAR", "[A-z]");
+		NFA nfa2 = new NFA("$Upper", "[^a-z]", nfa);
+		nfa2.print();
+	}
 
 }
