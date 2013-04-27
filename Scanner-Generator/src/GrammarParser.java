7b628100f2d942464ee0ff50aebfc47ad3331921
Matt Dye <matt@lawn-128-61-31-142.lawn.gatech.edu>
2013-04-27 16:26:24 -0400

    First set added

--- Scanner-Generator/src/GrammarParser.java
+++ Scanner-Generator/src/GrammarParser.java
@@ -31,9 +31,64 @@ public class GrammarParser {
 			rules.put(left, r);
 			
 		}
+		HashMap<String, ArrayList<String>> temp =  FirstSet(rules);
+		
 	}
-	
-	public HashMap<String, ArrayList<String>> FirstSet(HashMap<String, ArrayList<String>> rules){
+	public static ArrayList<String> first(String nonTerminal, HashMap<String, ArrayList<String>> rules){
+		ArrayList<String> out = new ArrayList<String>();
+		if(nonTerminal.equals("<epsilon>")){
+			out.add("<epsilon>");
+		}
+		else if(nonTerminal.length() > 0 && nonTerminal.charAt(0) == '<'){
+			for(String key : rules.keySet()){
+				if(key.equals(nonTerminal)){
+					for(String rule : rules.get(key)){
+						String[] arr = rule.split(" ");
+						ArrayList<String> temp = first(arr[0].trim(), rules);
+						for(String s : temp){
+							out.add(s);
+						}
+					}
+				}
+			}
+		}
+		else{
+			out.add(nonTerminal);
+		}
+		return out;
+	}
+	public static HashMap<String, ArrayList<String>> FirstSet(HashMap<String, ArrayList<String>> rules){
+		HashMap<String, ArrayList<String>> firstSet = new HashMap<String, ArrayList<String>>();
+		for(String s : rules.keySet()){
+			firstSet.put(s, new ArrayList<String>());
+			ArrayList<String> arr = first(s,rules);
+			for(String str : arr){
+				firstSet.get(s).add(str);
+			}
+		}
+		for(String s : rules.keySet()){
+			System.out.println("Key : " + s);
+			for(String str : firstSet.get(s)){
+				System.out.print(str + " ");
+			}
+			System.out.println(" ");
+			
+		}
+		/*iterate 1000 times!
+		for(String key : rules.keySet()){
+			for(String rule : rules.get(key)){
+				int k = 1;
+				String[] splitRule = rule.split(" ");
+				if(firstSet.get(key).size() > 0){
+					
+				}
+				else{
+					ArrayList<String> arr = first(key,rules);
+					firstSet.get(key).add
+				}
+			}
+		}
+		*/
 		return null;
 	}
 	
