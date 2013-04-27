3e7c362138e6f903ed6506dfedfab3c226df6b52
naomi6 <nrobert666@gmail.com>
2013-04-27 15:20:48 -0400

    Added first and follow sets

--- Scanner-Generator/src/GrammarParser.java
+++ Scanner-Generator/src/GrammarParser.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -11,11 +11,35 @@ public class GrammarParser {
 		BufferedReader grammarReader = new BufferedReader(new FileReader(grammarFile));
 		
 		String currLine;
+		
+		HashMap<String, ArrayList<String>> rules = new HashMap<String, ArrayList<String>>();
 	
 		while ((currLine = grammarReader.readLine()) != null){
-			String[] line = currLine.split("::=");
-			String[] right = line[1].split(" ");
+			String[] line = currLine.split(" ::= ");
+			String left = line[0];
+			String[] right = line[1].split(" \\| ");
+			
+			ArrayList<String> r = new ArrayList<String>();
+			for(String rule: right){
+				r.add(rule);
+			}
+			if(rules.get(left) != null){
+				for(String alreadyRule : rules.get(left)){
+					r.add(alreadyRule);
+				}
+			}
+			rules.put(left, r);
 			
 		}
 	}
+	
+	public HashMap<String, ArrayList<String>> FirstSet(HashMap<String, ArrayList<String>> rules){
+		return null;
+	}
+	
+	public HashMap<String, ArrayList<String>> FollowSet(HashMap<String, ArrayList<String>> rules){
+		return null;
+	}
+	
+	
 }
